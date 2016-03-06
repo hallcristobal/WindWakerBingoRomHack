9fa201d67c9e7868e9b5eeeec3e7abf9d98fa188
Christopher Serr <cryze92@gmail.com>
2016-03-06 22:41:03 +0100

    Update libtww and the example code

--- src/src/lib.rs
+++ src/src/lib.rs
@@ -4,9 +4,10 @@
 #[macro_use]
 extern crate libtww;
 
-use libtww::{controller, items, console, link, system};
-
-static mut item: u8 = 0;
+use libtww::prelude::*;
+use libtww::system;
+use libtww::Link;
+use libtww::game::Console;
 
 #[no_mangle]
 #[inline(never)]
@@ -14,27 +15,23 @@ pub extern "C" fn init() {
     // Call overriden instruction
     system::cdyl_init_async();
 
-    console::activate();
+    Console::get().setup();
 }
 
 #[no_mangle]
 #[inline(never)]
 pub extern "C" fn game_loop() {
-    console::write_line1(format!("Hello World: {:02X}", unsafe { item }));
+    let link = Link::get();
+    let console = Console::get();
+    let mut lines = &mut console.lines;
 
-    if controller::is_pressed(controller::DPAD_LEFT) {
-        unsafe {
-            item -= 1;
-        }
-    }
-    if controller::is_pressed(controller::DPAD_RIGHT) {
-        unsafe {
-            item += 1;
-        }
-    }
-    if controller::is_pressed(controller::DPAD_DOWN) {
-        items::spawn(link::get_position(), unsafe { item });
-    }
+    let _ = write!(lines[0].begin(), "Heart Pieces:   {}", link.heart_pieces);
+    let _ = write!(lines[1].begin(), "Heart Quarters: {}", link.heart_quarters);
+    let _ = write!(lines[2].begin(), "Rupees:         {}", link.rupees);
+    let _ = write!(lines[3].begin(), "Sword ID:       {:02X}", link.sword_id);
+    let _ = write!(lines[4].begin(), "Shield ID:      {:02X}", link.shield_id);
+    let _ = write!(lines[5].begin(), "Max Magic:      {}", link.max_magic);
+    let _ = write!(lines[6].begin(), "Magic:          {}", link.magic);
 }
 
 #[no_mangle]
